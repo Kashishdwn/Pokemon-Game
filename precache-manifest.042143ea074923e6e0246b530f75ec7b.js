3e667eecd0bb4e1d1f414ca26943e3a82b602077
kashish19 <kashishtheboy@gmail.com>
2020-04-12 03:53:48 +0530

    Updates

---
+++ precache-manifest.042143ea074923e6e0246b530f75ec7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4379957ebc61ef6bd2d7dbc21fb223f9",
+    "url": "/Pokemon-Game/index.html"
+  },
+  {
+    "revision": "5f30eb419b3ab713d165",
+    "url": "/Pokemon-Game/static/css/main.a2737a31.chunk.css"
+  },
+  {
+    "revision": "626c759c5ff57c9b9089",
+    "url": "/Pokemon-Game/static/js/2.7a858184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon-Game/static/js/2.7a858184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f30eb419b3ab713d165",
+    "url": "/Pokemon-Game/static/js/main.bf0933e3.chunk.js"
+  },
+  {
+    "revision": "610b7f1a0ec2d7daf901",
+    "url": "/Pokemon-Game/static/js/runtime-main.793cb724.js"
+  }
+]);
